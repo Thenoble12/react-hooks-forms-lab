589ed9c628321dabf901d41c2b0d03c8356d6cf8
Elijah Noble <elijahnobledev@gmail.com>
2022-06-05 21:29:11 -0700

    first commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Filter({ onCategoryChange }) {
+
+  const [category, setCategory] = useState('All')
+
+  function handleChange(e) {
+    e.preventDefault()
+    setCategory(e.target.value)
+  }
+
+
   return (
     <div className="Filter">
       <input type="text" name="search" placeholder="Search..." />
